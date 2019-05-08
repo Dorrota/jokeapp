f859104999627f4e0f0970a691642e13fce4b5cf
Dorro <doko@ailleron.com>
2019-05-08 14:59:27 +0200

    Controller GetByName added

--- src/main/java/guru/springframework/joke/controllers/PersonController.java
+++ src/main/java/guru/springframework/joke/controllers/PersonController.java
@@ -3,6 +3,7 @@ package guru.springframework.joke.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,15 @@ public class PersonController {
         return personList;
 	}
 	
-	
+	@RequestMapping(value="/name/{name}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Person getOnePersonByName (@PathVariable String name) {
+		
+		name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+		Person fullName = personService.getPersonByName(name);
+		
+		return fullName;
+		
+	}
 	
 
 }
